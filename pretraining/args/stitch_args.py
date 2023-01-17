acf4ed55964e65c9e3b11366ff653b04b0ac935f
friendshipkim <kwj962004@gmail.com>
2023-01-17 18:13:04 -0500

    add stitch arguments

---
+++ pretraining/args/stitch_args.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+@dataclass
+class StitchArguments:
+    """
+    Model stitching arguments
+    """
+
+    _argument_group_name = "Stitch Arguments"
+    
+    do_stitch: Optional[bool] = field(
+        default=False, metadata={"help": "weather to stitch two source models"}
+    )
+    src_model1_path: Optional[str] = field(
+        default=None, metadata={"help": "Path to the first source pretrained model"},
+    )
+    src_model2_path: Optional[str] = field(
+        default=None, metadata={"help": "Path to the second source pretrained model"},
+    )
